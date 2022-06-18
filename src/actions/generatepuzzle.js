1686b48a287ee6bc348b2742ce3d7db3a249e513
User <sean.al.baroudi@gmail.com>
2022-06-18 23:08:55 +0100

    Basic gen algorithm working better...still some bugs and refactoring.

--- src/actions/generatepuzzle.js
+++ src/actions/generatepuzzle.js
@@ -1,63 +1,67 @@
 
-const plus = function(curTotal,lim) {
-  return (curTotal+randNum(lim));
+//it is better to just pass two args back.
+//quite a few functions have their own special constraints.
+//let's just keep it all in the operation functions - easier that way.
+
+const glimit = 30;
+
+const plus = function(curTotal) {
+  const addend = randNum(glimit);
+  return {num:addend,result:curTotal+addend};
 }
 
-
-const times = function(curTotal,lim) {
-  return (curTotal*randNum(lim));
+const times = function(curTotal) {
+  const multiplicand = randNum(glimit)
+  return {num:multiplicand,result:curTotal*multiplicand};
 }
 
-const pluspercent = function(curTotal,lim) {
-  //not correct. but leave it for now.
-  return (curTotal+randNum(lim));
+const pluspercent = function(curTotal) {
+  const percent = randNum(10)*10; //left in percent notation
+  return {num:percent,result:(curTotal + (curTotal*(percent/100)))};
 }
 
-const square = function(curTotal,lim) {
-  return curTotal**2;
+const square = function(curTotal) {
+  return {num:null,result:curTotal**2}; //just catch with if statement later.
 }
 
-const cube = function(curTotal,lim) {
-  return curTotal**3;
+const cube = function(curTotal) {
+  return {num:null,result:curTotal**3};
 }
 
-const minus = function(curTotal,lim) {
-  return (curTotal-randNum(lim));
+const minus = function(curTotal) {
+  const subtractand = randNum(glimit)
+  return {num:subtractand,result:curTotal-subtractand};
 }
 
-const divide = function(curTotal,lim) {
-  return (curTotal/randNum(10));
+const divide = function(curTotal) {
+  const divisor = randNum(Math.round(curTotal/2))
+  return {num:divisor,result:curTotal/divisor};
 }
 
-const abfrac = function(curTotal,lim) {
-  //Template implementation - fix up later.
-  return (curTotal/randNum(10));
+const abfrac = function(curTotal) {
+  const denominator = randNum(Math.round(glimit))
+  const numerator = randNum(denominator); //kinda hacky..but one of a kind function.
+  return {num:numerator,denum:denominator,result:curTotal*(numerator/denominator)};
 }
 
-const minuspercent = function(curTotal,lim) {
-  //Not implemented right, fix up later
-  return (curTotal-randNum(10));
+const minuspercent = function(curTotal) {
+  const percent = randNum(10)*10; //left in percent notation
+  return {num:percent,result:(curTotal - (curTotal*(percent/100)))};
 }
 
-const squareroot = function(curTotal,lim) {
-  return (Math.sqrt(curTotal));
+const squareroot = function(curTotal) {
+  return {num:null,result:Math.sqrt(curTotal)};
 }
 
-const cuberoot = function(curTotal,lim) {
-  return (Math.cbrt(curTotal));
+const cuberoot = function(curTotal) {
+  return {num:null,result:Math.cbrt(curTotal)};
 }
 
 //Taken from https://www.freecodecamp.org/news/js-basics-how-to-reverse-a-number-9aefc20afa8d/
-const reversedigits = function(curTotal,limit) {
-  return (
-    parseFloat(
-      curTotal
-        .toString()
-        .split('')
-        .reverse()
-        .join('')
-    )
-  )
+const reversedigits = function(curTotal) {
+  return {num:null,
+    result: parseFloat(curTotal.toString().split('').reverse().join('')),
+    denum:null};
 }
 
 const nameMap = {"+":plus,
@@ -144,46 +148,67 @@ const randNP = function(limit = 10) {
   return candidateNum;
 };
 
-// This function takes our current running total, and a limit,
-// and tries to find something that will work.
-// our result must be a whole number > 1 by the end.
-const opSelectChecker = function(op,rt) {
-  const limit = 20; //just for now
-  //note: abfrac and reverse digits don't take two arguements - they are dummied for now.
-  for (let i = 2; i < limit; i++) {
-    let result = op(rt,limit);
-    if ((result % 1 == 0)&&(result > 0)) { return {num:i,result:result};}
-  }
-  return [-1,-1];
-}
-
-const genStepString = function(opStr, num) {
-  return (opStr + num); //string concat.
-}
-
-const calcNovNums = function() {
-  const startNumber = randNP(500);
-  let runningTotal = startNumber;
-  const opsArray = [];
-  let currOps = "";
-  while (opsArray.length < 4) {
-    currOps = opTypeSelect();
-    let values = opSelectChecker(nameMap[currOps],runningTotal);
-    if (values.result > 0) {
-      opsArray.push(genStepString(currOps, values.num));
-      runningTotal = values.result;
-    }
-  }
-  return opsArray;
-};
-
 //PMF function was used to calculate our random bounds, in selecting
 //operations. It had the right profile.
-/*
 const fCGeometric = function(lim) {
   let p = 0.45
   for (let i = 1; i <= lim; i++) {
     console.log("i = " + i + ", f(i) = " + (p*((1-p)**(i-1))));
   }
 };
-*/
+
+// This function takes our current running total, and a limit,
+// and tries to find something that will work.
+// our result must be a whole number > 1 by the end.
+const opSelectChecker = function(op,rt) {
+  //loop limit - try a certain number of times.
+  for (let i = 0; i < 10; i++) {
+    let { num, result, denum } = op(rt);
+    if ((result % 1 == 0)&&(result > 0)&&(result != rt)) { //Then our next operation was successful, package result.
+      return {num:num,result:result,denum:denum}; //denum will almost never be there. Let string funciton sort it out.
+    }
+  }
+  return {num:-1,result:-1,denum:-1}; //failure.
+}
+
+const genStepString = function(opname,values) {
+  let {num, denum} = values;
+  let retString = "";
+  if (num) { //operation with one numerator. (plus, divide, etc...)
+    retString = opname + ` ${num}`;
+  }
+  else { //non numerator op (square, cuberoot, etc...)
+    retString = opname;
+  }
+  //Special Cases.
+  if (denum) { //then we have an abfrac.
+      retString = `* (${num} / ${denum})`;
+  }
+  else if (opname == "-%") {
+      retString = `- ${num} %`;
+  }
+  else if (opname == "+%") {
+      retString = `+ ${num} %`;
+  }
+  return retString;
+}
+
+const calcNovNums = function() {
+  const startNumber = randNP(30);
+  console.log(startNumber);
+  var runningTotal = startNumber;
+  var currOps = "";
+  var values;
+  const opsArray = [];
+  const rTArray = [];
+  while (opsArray.length < 4) {
+    currOps = opTypeSelect();
+    values = opSelectChecker(nameMap[currOps],runningTotal);
+    if (values.result > 0) { //we must process our result for FE.
+      opsArray.push(genStepString(currOps,values));
+      rTArray.push(values.result);
+      runningTotal = values.result;
+    }
+  }
+  return {rTArray:rTArray,runningTotal:runningTotal,opsArray:opsArray};
+};
